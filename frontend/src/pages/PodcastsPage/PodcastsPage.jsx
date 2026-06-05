b391c2f30ee6d62f6fecad086fa305ff963c055d
Goldenschemeafrica <your-email@example.com>
2026-06-05 23:34:58 +0300

    Replace YouTube video with regular video (no live stream)

--- frontend/src/pages/PodcastsPage/PodcastsPage.jsx
+++ frontend/src/pages/PodcastsPage/PodcastsPage.jsx
@@ -295,7 +295,7 @@ const PodcastPage = () => {
                 <iframe
                   width="100%"
                   height="100%"
-                  src="https://www.youtube.com/embed/jfKfPfyJRdk"
+                  src="https://www.youtube.com/embed/LXb3EKWsInQ"
                   title="YouTube video player"
                   frameBorder="0"
                   allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
